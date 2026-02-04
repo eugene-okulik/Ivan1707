b496a6bddb3a67a39e522bf4785a68baed471ad0
Ivan Anohin <vanya.anokhin.2015@mail.ru>
2026-02-04 20:03:38 +0300

    fixed str 6, 10, 14, 20, 21, 22

--- homework/IvanAnohin/Homework 5/main.py
+++ homework/IvanAnohin/Homework 5/main.py
@@ -3,15 +3,15 @@ name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 
 text = "результат операции: 42"
-number = int(text.split(": ")[-1])
+number = int(text[text.index(':') + 2:])
 print(number + 10)
 
 text = "результат операции: 514"
-number = int(text.split(": ")[-1])
+number = int(text[text.index(':') + 2:])
 print(number + 10)
 
 text = "результат работы программы: 9"
-number = int(text.split(": ")[-1])
+number = int(text[text.index(':') + 2:])
 print(number + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
