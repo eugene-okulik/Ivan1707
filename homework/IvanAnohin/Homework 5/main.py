b5813dbd418e1564eb48d3670a9a159242bd3b99
Ivan Anohin <vanya.anokhin.2015@mail.ru>
2026-02-04 17:26:01 +0300

    fixed str 6,10,14,20,21,22

--- homework/IvanAnohin/Homework 5/main.py
+++ homework/IvanAnohin/Homework 5/main.py
@@ -3,21 +3,20 @@ name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 
 text = "результат операции: 42"
-number = int(text.split(": ")[1])
+number = int(text.split(": ")[-1])
 print(number + 10)
 
 text = "результат операции: 514"
-number = int(text.split(": ")[1])
+number = int(text.split(": ")[-1])
 print(number + 10)
 
 text = "результат работы программы: 9"
-number = int(text.split(": ")[1])
+number = int(text.split(": ")[-1])
 print(number + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-students_str = students[0] + ', ' + students[1] + ', ' + students[2]
-subjects_str = subjects[0] + ', ' + subjects[1] + ', ' + subjects[2]
-
-print(f'Students {students_str} study these subjects: {subjects_str}')
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+print('Students', students, 'study these subjects:', 'subjects', subjects)
