b9bbd2e064ea3c1a50e1413279f3abea965c5b5a
Ivan Anohin <vanya.anokhin.2015@mail.ru>
2026-02-07 16:34:46 +0300

    Homework 6 done

--- homework/IvanAnohin/Homework 6/main.py
+++ homework/IvanAnohin/Homework 6/main.py
@@ -1,23 +1,20 @@
 text = (
-   "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
-   "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+    "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 )
-
 
 words = text.split()
 final_words = []
 
-
 for word in words:
-   if word[-1] in ',.':
-       unsigned_word = word[:-1]
-       punctuation = word[-1]
-       new_word = unsigned_word + 'ing' + punctuation
-       final_words.append(new_word)
-   else:
-       new_word = word + 'ing'
-       final_words.append(new_word)
-
+    if word[-1] in ',.':
+        unsigned_word = word[:-1]
+        punctuation = word[-1]
+        new_word = unsigned_word + 'ing' + punctuation
+        final_words.append(new_word)
+    else:
+        new_word = word + 'ing'
+        final_words.append(new_word)
 
 result_text = ' '.join(final_words)
 print(result_text)
